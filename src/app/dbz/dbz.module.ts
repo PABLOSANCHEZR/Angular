ef6a43b288e8810fe76b972ad02503d31f47a35e
[PABLOSANCHEZR] <[pablo.sanchezr@viewnext.com]>
2021-11-12 09:21:59 +0100

    TODO OK

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 import { MainpageComponent } from './mainpage/mainpage.component';
 import { FormsModule } from '@angular/forms';
 import { PersonajesComponent } from './personajes/personajes.component';
+import { AgregarComponent } from './agregar/agregar.component';
+import { DbzService } from './services/dbz.service';
 
 
 
 @NgModule({
   declarations: [
     MainpageComponent,
-    PersonajesComponent
+    PersonajesComponent,
+    AgregarComponent
   ],
 
   exports:[
@@ -21,6 +24,11 @@ import { PersonajesComponent } from './personajes/personajes.component';
   imports: [
     CommonModule,
     FormsModule
+  ],
+
+  providers: [
+
+    DbzService
   ]
 })
 export class DbzModule { }
--- src/app/dbz/mainpage/mainpage.component.ts
+++ src/app/dbz/mainpage/mainpage.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
 
 
 
@@ -11,40 +12,16 @@ import { Personaje } from '../interfaces/dbz.interface';
 })
 export class MainpageComponent {
 
-  personajes: Personaje[] = [{
-    nombre: 'Goku',
-    poder: 15000
-  },
+      nuevo: Personaje = {
+        nombre : 'Mestro Roshi',
+        poder: 1000
+      }
 
-  {
-    nombre: 'Vegeta',
-    poder: 7500
-  }
+      constructor( private DbzService: DbzService){
+       }
 
-];
 
-  nuevo: Personaje= {
-
-    nombre: '',
-    poder: 0,
-
-  }
-      agregar() {
-
-        if(this.nuevo.nombre.trim().length === 0){
-          return;
-
-        }
-    
-    console.log(this.nuevo);
-
-    this.personajes.push(this.nuevo);
-    this.nuevo = {
-      nombre: '',
-      poder: 0
     }
-  }
-  }
 
 
 
